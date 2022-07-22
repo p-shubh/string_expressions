d43f439602246158d77ca7dac0baf87c77867e28
shubham prajapati <shubhamprajapati032@gmail.com>
2022-07-22 12:28:27 +0530

    more knowledge required to solve this problem

--- Expressions.go
+++ Expressions.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -36,4 +39,39 @@ func Expressionss(c *gin.Context) {
 		c.JSON(http.StatusOK, res)
 
 	}
+
+	str1 := strings.Split(reqBody.Data, "")
+
+	D, _ := fmt.Println("str1", str1)
+
+	a := str1[0]
+	b := str1[1]
+	d := str1[2]
+
+	fmt.Println("abc", a, b, d)
+
+	res := gin.H{
+		// "error":  err.Error(),
+		"result": D,
+	}
+	c.JSON(http.StatusOK, res)
+
+	strInt, _ := strconv.Atoi(str1[0])
+
+	// for i := 0; i < len(str1); i++ {
+	// 	if str1[i]=="+" {
+	// 		"+"= +
+	// 	}
+	// }
+
+	var retur bool = true
+
+	for i := 0; i <= 9; i++ {
+		if strInt == i {
+			retur = true
+		}
+	}
+
+	fmt.Println("retur :", retur, str1)
+
 }
